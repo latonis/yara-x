5b8fe12909ffc1501a66954c452b636e21324d78
Jacob Latonis <jlatonis@me.com>
2026-07-23 20:02:17 -0400

    feat: starting implementation of the eml module

--- lib/src/modules/eml/parser.rs
+++ lib/src/modules/eml/parser.rs
@@ -1,6 +1,6 @@
 use std::mem;
 
-use crate::modules::protos::eml::{Eml, Header};
+use crate::modules::protos::eml::{Eml, EmlPart, Header};
 use base64::prelude::*;
 use bstr::ByteSlice;
 use indexmap::IndexMap;
@@ -29,63 +29,78 @@ impl EmlParser {
     ) -> Result<Eml, Err<nom::error::Error<&'a [u8]>>> {
         self.result.is_eml = Some(true);
 
-        let (top_header, body) = self.split_message(input);
-
         // stack for processing
-        // let mut stack: Vec<&[u8]> = vec![input];
-        // while let Some(current_data) = stack.pop() {}
+        let mut stack: Vec<&[u8]> = vec![input];
+        let mut is_root = true;
+        // keep track to prevent mime bomb shenanigans
+        let mut parts_processed = 0;
 
-        let headers = self.parse_headers(top_header);
+        while let Some(current_data) = stack.pop() {
+            parts_processed += 1;
+            if parts_processed > 100 {
+                break;
+            }
 
-        for (key, value) in &headers {
-            self.result.headers.push(Header {
-                key: Some(key.to_vec()),
-                value: Some(value.to_vec()),
-                ..Default::default()
-            });
-        }
+            let (header, body) = self.split_message(current_data);
+            let headers = self.parse_headers(header);
 
-        // we parsed the initial headers, now we need take a look at the body/boundaries
-        // Content-Type should be checked for multipart and boundary
-        if let Some(content_type) = headers.get(&b"Content-Type"[..]) {
-            // check if starts with multipart/
-            if content_type.starts_with(b"multipart/") {
-                if let Some(pos) = content_type.find("boundary=") {
-                    // if so, find pos of boundary=
-                    let start = pos + "boundary=".len();
-                    let remainder = content_type[start..].trim();
+            if is_root {
+                self.result.headers = self.map_to_proto_headers(&headers);
+                self.result.body = Some(body.to_vec());
+                self.result.decoded_body = self.decode_body(&headers, body);
+            }
 
-                    // value could be "value" or value
-                    let boundary_bytes = if remainder.starts_with(b"\"") {
-                        // enclosed by ""
-                        remainder.split_str("\"").nth(1).unwrap_or(&[])
-                    } else {
-                        // no quotes but may be multiple items in here, split and grab 0th
-                        remainder.split_str(";").next().unwrap_or(&[]).trim()
-                    };
+            // Content-Type should be checked for multipart and boundary
+            let mut found_boundary = None;
+            if let Some(content_type) = headers.get(&b"Content-Type"[..]) {
+                // check if starts with multipart/
+                if content_type.starts_with(b"multipart/") {
+                    if let Some(pos) = content_type.find("boundary=") {
+                        // if so, find pos of boundary=
+                        let start = pos + "boundary=".len();
+                        let remainder = content_type[start..].trim();
 
-                    dbg!(boundary_bytes.to_str_lossy());
+                        // value could be "value" or value
+                        let boundary_bytes = if remainder.starts_with(b"\"") {
+                            // enclosed by ""
+                            remainder.split_str("\"").nth(1).unwrap_or(&[])
+                        } else {
+                            // no quotes but may be multiple items in here, split and grab 0th
+                            remainder
+                                .split_str(";")
+                                .next()
+                                .unwrap_or(&[])
+                                .trim()
+                        };
+
+                        if !boundary_bytes.is_empty() {
+                            let mut delimiter = b"--".to_vec();
+                            delimiter.extend_from_slice(boundary_bytes);
+                            found_boundary = Some(delimiter);
+                        }
+                    }
                 }
             }
-        }
 
-        self.result.body = Some(body.trim().to_vec());
+            if let Some(delimiter) = found_boundary {
+                let parts: Vec<&[u8]> = body.split_str(&delimiter).collect();
+                for part in parts.into_iter().rev() {
+                    let trimmed = part.trim();
 
-        // check if headers contain Content-Transfer-Encoding
-        if let Some(content_encoding) =
-            headers.get(&b"Content-Transfer-Encoding"[..])
-        {
-            if content_encoding == &b"base64"[..] {
-                let cleaned_body: Vec<u8> = body
-                    .iter()
-                    .filter(|&&b| !b.is_ascii_whitespace())
-                    .cloned()
-                    .collect();
-
-                let decoded_content =
-                    BASE64_STANDARD.decode(cleaned_body).unwrap();
-                self.result.decoded_body = Some(decoded_content);
+                    if trimmed.is_empty() || trimmed.starts_with(b"--") {
+                        continue;
+                    }
+                    stack.push(trimmed);
+                }
+            } else if !is_root {
+                self.result.parts.push(EmlPart {
+                    headers: self.map_to_proto_headers(&headers),
+                    body: Some(body.to_vec()),
+                    decoded_body: self.decode_body(&headers, body),
+                    ..Default::default()
+                });
             }
+            is_root = false;
         }
 
         Ok(mem::take(&mut self.result))
@@ -99,6 +114,48 @@ impl EmlParser {
         } else {
             (input, &[][..])
         }
+    }
+
+    fn decode_body(
+        &self,
+        headers: &IndexMap<&[u8], Vec<u8>>,
+        body: &[u8],
+    ) -> Option<Vec<u8>> {
+        let content_encoding =
+            headers.get(&b"Content-Transfer-Encoding"[..])?;
+
+        if content_encoding == &b"base64"[..] {
+            let cleaned_body: Vec<u8> = body
+                .iter()
+                .filter(|&&b| !b.is_ascii_whitespace())
+                .cloned()
+                .collect();
+
+            return BASE64_STANDARD.decode(cleaned_body).ok();
+        }
+
+        if content_encoding == &b"Quoted-Printable"[..] {
+            return quoted_printable::decode(
+                body,
+                quoted_printable::ParseMode::Robust,
+            )
+            .ok();
+        }
+        None
+    }
+
+    fn map_to_proto_headers(
+        &self,
+        headers: &IndexMap<&[u8], Vec<u8>>,
+    ) -> Vec<Header> {
+        headers
+            .iter()
+            .map(|(k, v)| Header {
+                key: Some(k.to_vec()),
+                value: Some(v.to_vec()),
+                ..Default::default()
+            })
+            .collect()
     }
 
     fn parse_headers<'a>(
